8199b8684a33510b3945f8d5843b9ce888724135
Thomas Ward <teward@ubuntu.com>
2018-09-16 15:25:15 -0400

    Regex match to determine if attempting to hit a .onion address
    Since we do not support .onions, disallowing them w/ error is a good approach.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from app.weapons.singleshot import SingleShotFactory
 from app.weapons.fullauto import FullAutoFactory
 from app.weapons.slowloris import SlowLorisFactory
 import fire
+import regex
 import signal
 import sys
 
@@ -57,6 +58,9 @@ class CLI:
                 cache_buster=self._cache_buster
             )
 
+            if regex.match(r'.*\.onion$', target, regex.I):
+                raise NotImplementedError("This application does not support .onion addresses.")
+
             self._platoon.attack(
                 weapon_factory=weapon_factory,
                 target_url=target
@@ -84,6 +88,9 @@ class CLI:
                 http_method=self._http_method,
                 cache_buster=self._cache_buster
             )
+
+            if regex.match(r'.*\.onion$', target, regex.I):
+                raise NotImplementedError("This application does not support .onion addresses.")
 
             self._platoon.attack(
                 weapon_factory=weapon_factory,
@@ -114,6 +121,9 @@ class CLI:
                 cache_buster=self._cache_buster,
                 num_sockets=num_sockets
             )
+
+            if regex.match(r'.*\.onion$', target, regex.I):
+                raise NotImplementedError("This application does not support .onion addresses.")
 
             self._platoon.attack(
                 weapon_factory=weapon_factory,
